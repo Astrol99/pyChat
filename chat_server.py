7f00951bcd96594196286f675fd6c9c9e139bf7a
Astr0l <41875791+Astr0l@users.noreply.github.com>
2019-05-09 18:19:15 -0500

    Preperations for sockets

--- chat_server.py
+++ chat_server.py
@@ -4,3 +4,11 @@ import select
 HEADER_LENGTH = 10
 IP = "127.0.0.1"
 PORT = 6969
+
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.setsockopt(socket.SOCK_SEQPACKET, socket.SO_REUSEADDR, 1)
+
+server_socket.bind((IP, PORT))
+server_socket.listen(5)
+
+socket_list = [server_socket]
