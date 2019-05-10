e709805d7e25abd33fab08a83ca75483dec67fe0
Astr0l <41875791+Astr0l@users.noreply.github.com>
2019-05-09 19:05:17 -0500

    Added user inputs for client

--- chat_client.py
+++ chat_client.py
@@ -1,1 +1,9 @@
 import socket
+import select
+import errno
+
+HEADER_LENGTH = 10
+
+IP = input("[*] Enter server ip: ")
+PORT = int(input("[*] Enter server port: ")) 
+USERNAME = input("[*] Enter your username: ")
