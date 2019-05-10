dc48d6103c4735a9aca214f076848c11684fbbf7
Astr0l <41875791+Astr0l@users.noreply.github.com>
2019-05-10 14:52:36 -0500

    Made chat_client to only send messages

--- client/chat_client.py
+++ client/chat_client.py
@@ -25,30 +25,3 @@ while True:
         message = message.encode('utf-8')
         message_header = f"{len(message) :< {HEADER_LENGTH}}".encode('utf-8')
         clientSocket.send(message_header+message)
-    
-    try:
-        while True:
-            # Recv incoming msgs
-            usernameHeader = clientSocket.recv(HEADER_LENGTH)
-            if not len(usernameHeader):
-                print("[-] Server closed -> Disconnecting...")
-                sys.exit()
-            
-            usernameLength = int(usernameHeader.decode('utf-8').strip())
-            username = clientSocket.recv(usernameLength).decode('utf-8')
-
-            message_header = clientSocket.recv(HEADER_LENGTH)
-            message_length = int(message_header.decode('utf-8').strip())
-            message = clientSocket.recv(message_length).decode('utf-8')
-
-            print(f"{username}: {message}")
-
-    except IOError as e:
-        if e.errno != errno.EAGAIN and e.errno != errno.EWOULDBLOCK:
-            print('[!] Reading error', str(e))
-            sys.exit()
-        continue
-
-    except Exception as e:
-        print("[!] General Error", str(e))
-        sys.exit()
